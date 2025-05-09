1f90389d2b89f09b948ade2ed5f0140762978482
Gus Ariotti <gustavo.ariotti@gmail.com>
2025-05-09 17:33:51 -0300

    index3.js modified

--- index3.js
+++ index3.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-console.log('hola');
-console.log('mundo');
-=======
-console.log('hola 34');
-console.log('hola 33');
-console.log('hola 54');
->>>>>>> 42e456452a3b234f638bb73a4cef99b86806ea10
+console.log('hola 1');
+console.log('hola 2');
+console.log('hola 3');
+
